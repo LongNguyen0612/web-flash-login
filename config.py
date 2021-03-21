931656b12fbca8a8869788ee62070121dc1d3b19
longnguyen0612 <nhatlong6122@gmail.com>
2021-03-21 16:21:38 +0700

    add-web-part1

---
+++ config.py
@@ -0,0 +1,14 @@
+class Config(object):
+    TESTING = False
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    SQLALCHEMY_ECHO = True
+
+class ProductionConfig(Config):
+    DEBUG = False
+
+app_config = {
+    'development': DevelopmentConfig,
+    'production': ProductionConfig
+}
---
+++ instance/config.py
@@ -0,0 +1,3 @@
+SECRET_KEY = 'root'
+SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:123456@localhost/postgres'
+SQLALCHEMY_TRACK_MODIFICATIONS = False
